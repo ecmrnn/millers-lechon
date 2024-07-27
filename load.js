d36e2eb0d48862718be576f9a2a2f71ba86951a5
Emanuelle Maranan <marananemanuelle@gmail.com>
2024-07-28 01:44:40 +0800

    content: added more ulam | style: updated checkbox styles

--- load.js
+++ load.js
@@ -1,10 +1,19 @@
 let ulam = [
-    ["Sinigang (Baboy)", ""],
-    ["Lechon Sisig", ""],
-    ["Dinuguan", ""],
-    ["Chopsuey", ""],
-    ["Ampalaya", ""],
+    "Sinigang (Baboy)",
+    "Lechon Sisig",
+    "Lechon Paksiw",
+    "Dinuguan",
+    "Chopsuey",
+    "Ampalaya",
+    "Menudo",
+    "Mechado",
+    "Hamonado",
+    "Isda",
+    "Bopis",
+    "Adobo (Baboy)",
 ];
+
+ulam.sort();
 
 let output = document.querySelector("#ulams");
 
@@ -13,26 +22,19 @@ ulam.forEach(u => {
     let input = document.createElement("input");
     let p = document.createElement("p");
 
-    label.setAttribute("for", u[0]);
-    label.classList.add("ulam-label");
-    label.classList.add("select-none");
-    label.classList.add("flex");
-    label.classList.add("items-center");
-    label.classList.add("gap-5");
+    let label_class = "ulam-label select-none flex items-center gap-5 border rounded-md px-3 py-2 cursor-pointer".split(' ');
+
+    label.setAttribute("for", u);
+    label.classList.add(...label_class);
 
     input.setAttribute("type", "checkbox");
-    input.setAttribute("id", u[0]);
+    input.setAttribute("id", u);
 
     p.classList.add("capitalize");
-    p.innerHTML = `<strong>${u[0]}</strong><span>${u[1]}</span>`;
+    p.innerHTML = `<strong>${u}</strong><span></span>`;
 
     label.appendChild(input)
     label.appendChild(p)
 
     output.appendChild(label)
-    console.log(output)
 });
-{/* <label for="<?= $ulam["ulam"] ?>" class="">
-    <input type="checkbox" name="ulam" id="<?= $ulam["ulam"] ?>">
-    <p class="capitalize"><strong class="ulam-name"><?= $ulam["ulam"] ?></strong> - <span><?= number_format($ulam["price"], 2) ?></span></p>
-</label> */}

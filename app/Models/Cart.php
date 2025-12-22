e8e4b706f0de4245a48495d80001020a8f9ee2f5
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-12-23 01:42:10 +0800

    🗃️  database(update): added cart_item model, migration, and relationship between cart_item and cart tables

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
@@ -15,5 +16,10 @@ class Cart extends Model
     public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class, 'converted_order_id');
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
     }
 }
---
+++ database/migrations/2025_12_22_173810_create_cart_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Cart;
+use App\Models\Freebie;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Cart::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Freebie::class)->nullable()->constrained()->cascadeOnDelete();
+            $table->integer('quantity')->default(1);
+            $table->double('weight')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};

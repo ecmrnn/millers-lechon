afeda455938f51e371592d59d853d79ea60924f8
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-12-23 01:37:11 +0800

    🗃️  database(update): added nullable cart_id on orders table

--- database/migrations/2025_12_17_160522_create_orders_table.php
+++ database/migrations/2025_12_17_160522_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Cart;
 use App\Models\Customer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -14,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Customer::class, 'customer_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Customer::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Cart::class)->nullable()->constrained()->cascadeOnDelete();
             // Order details
             $table->date('order_date');
             $table->time('order_time');
